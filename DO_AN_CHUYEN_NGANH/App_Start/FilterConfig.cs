a495e322261691b94cea2a11fa66dbf38a2561e9
vuvie <vuviethoangdakmil@gmail.com>
2022-12-03 18:32:13 +0700

    Add project files.

---
+++ DO_AN_CHUYEN_NGANH/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DO_AN_CHUYEN_NGANH
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DO_AN_CHUYEN_NGANH/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DO_AN_CHUYEN_NGANH.MvcApplication" Language="C#" %>
